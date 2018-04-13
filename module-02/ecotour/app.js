925aac479cd60e15dc10cd95a9a72a98f371f1a4
Pastushkov <dimaspas62@gmail.com>
2018-04-13 19:19:23 +0300

    Angular

--- module-02/ecotour/app.js
+++ module-02/ecotour/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var browserify = require('browserify-middleware');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var admin = require('./routes/admin');
+var apiV1 = require('./routes/apiv1');
 
 var app = express();
 
@@ -21,26 +25,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.get('/javascripts/bundle.js', browserify('./client/script.js'));
+var dbConnectionStr = 'mongodb://localhost:27017/ecotour2018';
+mongoose.connect(dbConnectionStr);
+
+if (app.get('env') == 'development') {
+    var browserSync = require('browser-sync');
+    var config = {
+        files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.hbs"],
+        logLevel: 'debug',
+        logSnippet: false,
+        reloadDelay: 3000,
+        reloadOnRestart: true
+    };
+    var bs = browserSync(config);
+    app.use(require('connect-browser-sync')(bs));
+}
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/admin', admin);
+app.use('/api/v1', apiV1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
