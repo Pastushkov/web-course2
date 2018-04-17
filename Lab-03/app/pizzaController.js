f3164492ad32ef9fe6ca675c49dbe93a55b9777a
Pastushkov <dimaspas62@gmail.com>
2018-04-17 21:22:18 +0300

    Angular Lab-03 and Lab-04

--- Lab-03/app/pizzaController.js
+++ Lab-03/app/pizzaController.js
@@ -1,11 +1,20 @@
-	app.controller('pizzaController', ['$scope', function($scope) {
-	    $scope.model = {
-	        title: 'Pizza Builder',
-	        availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
-	        toppings: []
-	    };
-	    $scope.addTopping = function(topping) {
-	        $scope.model.toppings.push(topping);
-	        $scope.model.search = null;
-	    }
-	}]);
+app.controller('pizzaController', ['$scope', function($scope) {
+    $scope.model = {
+        title: 'Pizza Builder',
+        availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
+        toppings: [],
+        isCloseAlert: true
+    };
+    $scope.addTopping = function(topping) {
+        $scope.model.toppings.push(topping);
+        $scope.model.isCloseAlert = false;
+        $scope.model.search = null;
+    }
+    $scope.closeAlert = function() {
+        $scope.model.isCloseAlert = true;
+    }
+    $scope.noResult = function() {
+        let res = $scope.model.availableToppings.filter(item => item.includes($scope.model.search));
+        return res.length < 1;
+    }
+}]);
